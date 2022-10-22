d85f9f914111c167faa5c5889567a3ea43552bef
Douglas Brandão <douglas-brand@hotmail.com>
2022-10-21 23:42:06 -0300

    SQL

---
+++ app/src/main/java/com/example/novoprojeto/FormCadastro.java
@@ -0,0 +1,56 @@
+package com.example.novoprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class FormCadastro extends AppCompatActivity {
+
+    private EditText edit_nome,edit_email,edit_senha;
+    private Button bt_cadastrar;
+    String[] mensagens = {"Preenchar todos os campos","Cadastro realizado com sucesso"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view,mensagens[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    CadastrarUsuario();
+                }
+            }
+        });
+    }
+    private void CadastrarUsuario(){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+    }
+    private void IniciarComponentes(){
+
+        edit_nome = findViewById(R.id.edit_nome);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_cadastrar = findViewById(R.id.bt_cadastrar);
+    }
+}
